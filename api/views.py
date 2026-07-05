37412e810909e4599dda0aeb667c650fcc61d0c5
Josephus Mabanag <69774348+blueConsole@users.noreply.github.com>
2026-07-06 00:54:26 +1000

    Implement error handling

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import ValidationError
 from .models import User, Ride, RideEvent
 from .serializers import UserSerializer, RideSerializer, RideEventSerializer
 from django.db.models import ExpressionWrapper, F, FloatField, Value, Prefetch
@@ -35,24 +35,38 @@ class RideViewSet(ModelViewSet):
             pickup_latitude = self.request.query_params.get('pickup_latitude', None)
             pickup_longitude = self.request.query_params.get('pickup_longitude', None)
 
-            if pickup_latitude is not None and pickup_longitude is not None:
+            if pickup_latitude is None or pickup_longitude is None:
+                raise ValidationError({
+                    'ordering': 'pickup_latitude and pickup_longitude are required when ordering=distance.'
+                })
+
+            try:
                 pickup_latitude = float(pickup_latitude)
                 pickup_longitude = float(pickup_longitude)
+            except ValueError:
+                raise ValidationError({
+                    'pickup_latitude': 'A valid number is required.',
+                    'pickup_longitude': 'A valid number is required.',
+                })
 
-                latitude_difference = F('pickup_latitude') - Value(pickup_latitude)
-                longitude_difference = F('pickup_longitude') - Value(pickup_longitude)
-                distance_expression = (
-                    latitude_difference * latitude_difference
-                ) + (
-                    longitude_difference * longitude_difference
+            latitude_difference = F('pickup_latitude') - Value(pickup_latitude)
+            longitude_difference = F('pickup_longitude') - Value(pickup_longitude)
+            distance_expression = (
+                latitude_difference * latitude_difference
+            ) + (
+                longitude_difference * longitude_difference
+            )
+
+            queryset = queryset.annotate(
+                distance=ExpressionWrapper(
+                    Sqrt(distance_expression),
+                    output_field=FloatField()
                 )
-
-                queryset = queryset.annotate(
-                    distance=ExpressionWrapper(
-                        Sqrt(distance_expression),
-                        output_field=FloatField()
-                    )
-                ).order_by('distance')
+            ).order_by('distance')
+        elif ordering is not None:
+            raise ValidationError({
+                'ordering': 'Supported values are pickup_time, -pickup_time, and distance.'
+            })
         else:
             queryset = queryset.order_by('id_ride')
         return queryset
