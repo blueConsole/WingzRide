642a39dc3af90408cded52f49daf4bed5b9ae037
Josephus Mabanag <69774348+blueConsole@users.noreply.github.com>
2026-07-05 21:49:16 +1000

    Optimize performance by adding indexes

--- api/models.py
+++ api/models.py
@@ -12,14 +12,14 @@ class User(AbstractUser):
 
 class Ride(models.Model):
     id_ride = models.AutoField(primary_key=True)
-    status = models.CharField(max_length=20, choices=[('en-route', 'En-route'), ('pickup', 'Pickup'), ('dropoff', 'Dropoff'), ('completed', 'Completed'), ('cancelled', 'Cancelled')])
+    status = models.CharField(max_length=20, choices=[('en-route', 'En-route'), ('pickup', 'Pickup'), ('dropoff', 'Dropoff'), ('completed', 'Completed'), ('cancelled', 'Cancelled')], db_index=True)
     id_rider = models.ForeignKey(User, related_name='rides_as_a_rider', db_column='id_rider', on_delete=models.PROTECT)
     id_driver = models.ForeignKey(User, related_name='rides_as_a_driver', db_column='id_driver', on_delete=models.PROTECT)
     pickup_latitude = models.FloatField()
     pickup_longitude = models.FloatField()
     dropoff_latitude = models.FloatField()
     dropoff_longitude = models.FloatField()
-    pickup_time = models.DateTimeField()
+    pickup_time = models.DateTimeField(db_index=True)
 
     def __str__(self):
         return f"Ride {self.id_ride} - {self.status}"
@@ -28,7 +28,7 @@ class RideEvent(models.Model):
     id_ride_event = models.AutoField(primary_key=True)
     id_ride = models.ForeignKey(Ride, related_name='events', db_column='id_ride', on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     def __str__(self):
         return f"Ride Event {self.id_ride_event} - {self.description}"
